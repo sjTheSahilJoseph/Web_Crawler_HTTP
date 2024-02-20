f82d08040110e0e87741c310f347a2aad0fe813a
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-20 19:19:16 +0500

    URL Normalization

--- src/crawl.js
+++ src/crawl.js
@@ -1,6 +1,14 @@
 function normalizeURL(url) {
+    const urlObject = new URL(url);    
     
-    return url;
+    let normalizedURL = `${urlObject.hostname}${urlObject.pathname}`;
+    
+    if (normalizedURL.length > 0 && normalizedURL.slice(-1) === '/') {
+        normalizedURL = normalizedURL.slice(0, -1);
+    }
+
+
+    return normalizedURL;
 }
 
 module.exports = {
