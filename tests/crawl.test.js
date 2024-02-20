05478037b1b126135da341505484b23f696d2299
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-20 19:16:30 +0500

    normalizeURL tests and logic

--- tests/crawl.test.js
+++ tests/crawl.test.js
@@ -1,9 +1,25 @@
 const {normalizeURL} = require("../src/crawl.js");
 const {test, expect} = require("@jest/globals");
 
-test('normalizeURL', ()=>{
-    const input = '';
+test('normalizeURL strip protocol', ()=>{
+    const input = 'https://josephprogrammingcompany.com/about';
     const actual = normalizeURL(input);
-    const expected = '';
+    const expected = 'josephprogrammingcompany.com/about';
     expect(actual).toEqual(expected);
 });
+
+test('normalizeURL strip trailing slash', ()=>{
+    const input = 'https://josephprogrammingcompany.com/about/';
+    const actual = normalizeURL(input);
+    const expected = 'josephprogrammingcompany.com/about';
+    expect(actual).toEqual(expected);
+});
+
+test('normalizeURL capitals', ()=>{
+    const input = 'https://josePhprOgramMingcompany.Com/about/';
+    const actual = normalizeURL(input);
+    const expected = 'josephprogrammingcompany.com/about';
+    expect(actual).toEqual(expected);
+});
+
+
