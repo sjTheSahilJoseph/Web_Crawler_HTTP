189fc8dfdabcdf3230b3507679e029b7aec00028
sjTheSahilJoseph <sjthesahiljoseph@gmail.com>
2024-02-21 18:21:32 +0500

    crawler

--- src/crawl.js
+++ src/crawl.js
@@ -1,4 +1,29 @@
-const {JSDOM} = require('jsdom');
+const { JSDOM } = require('jsdom');
+
+async function crawlPage(currentURL) {
+
+    console.log(`Actively Crawling: ${currentURL}`);
+
+    try {
+
+        const res = await fetch(currentURL);
+
+        if (res.status > 399) {
+            console.log("Error with fetch status code : " + res.status);
+            return;
+        }
+
+        const contentType = res.headers.get("content-type");
+        if (contentType.includes("text/html")) {
+            console.log("Error with content-type : " + contentType);
+        }
+
+        console.log(await res.text());
+    } catch (err) {
+        console.log(err.message);
+    }
+
+}
 
 function getURLsFromHTML(htmlBody, baseURL) {
     const urls = [];
@@ -7,12 +32,12 @@ function getURLsFromHTML(htmlBody, baseURL) {
     const linkElements = dom.window.document.querySelectorAll('a');
 
     for (const linkElement of linkElements) {
-        if (linkElement.href.slice(0,1) === '/') {
+        if (linkElement.href.slice(0, 1) === '/') {
             // Relative URL
             try {
 
-            const urlObject = new URL(`${baseURL}${linkElement.href}`);
-            urls.push(urlObject.href);
+                const urlObject = new URL(`${baseURL}${linkElement.href}`);
+                urls.push(urlObject.href);
             } catch (err) {
                 console.log(err.message);
             }
@@ -20,8 +45,8 @@ function getURLsFromHTML(htmlBody, baseURL) {
             // Absolute URL
             try {
 
-            const urlObject = new URL(linkElement.href);
-            urls.push(urlObject.href);
+                const urlObject = new URL(linkElement.href);
+                urls.push(urlObject.href);
             } catch (err) {
                 console.log(err.message);
             }
@@ -32,10 +57,10 @@ function getURLsFromHTML(htmlBody, baseURL) {
 }
 
 function normalizeURL(url) {
-    const urlObject = new URL(url);    
-    
+    const urlObject = new URL(url);
+
     let normalizedURL = `${urlObject.hostname}${urlObject.pathname}`;
-    
+
     if (normalizedURL.length > 0 && normalizedURL.slice(-1) === '/') {
         normalizedURL = normalizedURL.slice(0, -1);
     }
@@ -46,5 +71,6 @@ function normalizeURL(url) {
 
 module.exports = {
     normalizeURL,
-    getURLsFromHTML
+    getURLsFromHTML,
+    crawlPage
 };
--- src/main.js
+++ src/main.js
@@ -1,1 +1,25 @@
-console.log("hello");
+const {crawlPage} = require('./crawl.js');
+
+
+/** 
+ * process.argv.lenght < 3
+ *  Why?
+ *      Because basically it already has 1 argument which is the node interpreter.
+ *      then it has the name of the file.
+ *      not 3rd one is which we're providing on command line.
+ ***/
+function main() {
+    if (process.argv.length < 3) {
+        console.log("No website provided");
+        process.exit(1);
+    } else if (process.argv.length > 3) {
+        console.log("Too many command line args");
+        process.exit(1);
+    }
+
+    const baseURL = process.argv[2];
+    console.log(`Starting Crawl of ${baseURL}`);
+    crawlPage(baseURL);
+}
+
+main();
